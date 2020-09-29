2549308af44f8a1084573135076698fb751089d9
DosImpact <ypd03008@gmail.com>
2020-09-29 18:32:03 +0900

    add comment

--- lab02/src/file/MainTest.java
+++ lab02/src/file/MainTest.java
@@ -7,14 +7,15 @@ import java.util.List;
 public class MainTest {
 	
 	public ArrayList<FileInfo> fileList = new ArrayList<FileInfo>();
-	// 파일 info 배열 출력
+	
+	// 파일 info 리스트를 배열로 출력
 	public FileInfo[] printFileInfos() {
 		FileInfo[] fi = new FileInfo[this.fileList.size()];
 		fi = (FileInfo[]) this.fileList.toArray(fi);
 		return fi;
 	}
 	
-	// 파일 info objet get
+	// 파일 이름을 검색합니다.
 	public FileInfo getFileInfo(String filename) {
 		for(FileInfo f : fileList) {
 			if(f.getFilename() == filename) {
@@ -24,14 +25,14 @@ public class MainTest {
 		return null;
 	}
 
-	//get FileInfo
+	//디렉터리를 읽어서  모든 하위 디렉터리까지 파일 정보를 리스트로 저장합니다.
 	public List<FileInfo> getFileInfos(String dirPath){
 		// 경로의 파일과 디렉터리를 읽습니다.
 		File dir = new File(dirPath);
 		File[] sub =  dir.listFiles();
 		for(File s :sub) {
 			
-			// 디렉터리인 경우 해당 경로로 들어가서 파일과 디렉터리를 읽습니다.
+			// 디렉터리인 경우 해당 재귀호출을 통해 하위 경로로 들어가서 파일과 디렉터리를 읽습니다. 그리고 리스트에 추가합니다.
 			if(s.isDirectory()) {
 				System.out.println("<DIR>\t"+s.length()+"\t\t"+s.lastModified()+"\t\t"+s.getName());
 				String path = dirPath+"\\"+s.getName();
@@ -54,18 +55,22 @@ public class MainTest {
 		
 		MainTest mt = new MainTest();
 		
+		// 로컬 D 드라이브의 해당 경로에서 , 모든 파일과 디렉터리를 읽어서 리스트에 저장합니다.
 		mt.getFileInfos("D:\\Java_Lectrue\\Strategy");
-		System.out.println("\n\n원본 리스트");
 		
+		// 원본 리스트를 출력합니다.
+		System.out.println("\n\n원본 리스트");
 		FileInfo[]  fi = null ;
 		fi = mt.printFileInfos();
 		for(FileInfo f:fi) {
 			System.out.println(f);
 		}
 		
+		// sorter 객체를 생성합니다.
 		Sorter  sorter = new Sorter();
 		
 		
+		// sorter 객체에 , filename 으로 정렬하는 정책을 셋팅합니다. 정렬 후 결과값을 출력합니다.
 		System.out.println("\n\n파일 이름으로 정렬된 리스트");
 		sorter.setComparator(new FilenameComparator());
 		
@@ -75,7 +80,7 @@ public class MainTest {
 			System.out.println(f);
 		}
 		
-		
+		// sorter 객체에 , type 으로 정렬하는 정책을 셋팅합니다. 정렬 후 결과값을 출력합니다.
 		System.out.println("\n\n파일 종류로 정렬된 리스트");
 		sorter.setComparator(new TypeComparator());
 		
@@ -85,7 +90,7 @@ public class MainTest {
 			System.out.println(f);
 		}
 		
-		
+		// sorter 객체에 , size 으로 정렬하는 정책을 셋팅합니다. 정렬 후 결과값을 출력합니다.
 		System.out.println("\n\n파일 크기로 정렬된 리스트");
 		sorter.setComparator(new SizeComparator());
 		
@@ -95,7 +100,7 @@ public class MainTest {
 			System.out.println(f);
 		}
 		
-		
+		// sorter 객체에 , 마지막 수정시간 으로 정렬하는 정책을 셋팅합니다. 정렬 후 결과값을 출력합니다.
 		System.out.println("\n\n파일 수정 시간으로 정렬된 리스트");
 		sorter.setComparator(new DateComparator());
 		
--- lab02/src/file/Sorter.java
+++ lab02/src/file/Sorter.java
@@ -11,11 +11,6 @@ public class Sorter {
 		this.comparator = comparator;
 	}
 	
-//	public <E> void Sort(E data) {
-//		Arrays.<E>sort(data,null);
-//	}
-//	
-
 	
 	public void bubbleSort(FileInfo[] data ) {
 		for(int i = 0; i < data.length -1 ; i++) {
@@ -29,94 +24,5 @@ public class Sorter {
 		}
 	}
 	
-//	public <E> void bubbleSort(E[] data ) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( this.comparator.compare(data[j],data[j+1]) > 0 ) {
-//					E tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
 
-
-
-
-
-
-	
-//	
-//	public <E extends String > void bubbleSort(E[] data ) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if(  ) {
-//					E tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
-//	
-//	public <E extends Date > void bubbleSort(E[] data ) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( data[j].compareTo(data[j+1]) > 0) {
-//					E tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
-//	public <E extends Integer> void bubbleSort(E[] data ) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( data[j] > data[j + 1]) {
-//					E tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
-//	
-//	private void bubbleSortInts(int[] data) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( data[j] > data[j + 1]) {
-//					int tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
-//	
-//	private void bubbleSortStrings(String[] data) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( data[j].compareTo(data[j+1]) > 0) {
-//					String tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
-//	
-//	
-//	private void bubbleSortDates(Date[] data) {
-//		for(int i = 0; i < data.length -1 ; i++) {
-//			for(int j = 0 ; j < data.length - i - 1; j ++) {
-//				if( data[j].compareTo(data[j+1]) > 0) {
-//					Date tmp = data[j];
-//					data[j] = data[j+1];
-//					data[j+1] = tmp; 
-//				}
-//			}
-//		}
-//	}
 }
