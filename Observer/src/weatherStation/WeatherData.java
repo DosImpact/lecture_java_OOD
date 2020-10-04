345b7449c7498b9d1f4f058d29a26e2bcaa54449
Doyoung <doyoung@Doyoungs-MacBook-Pro.local>
2020-10-04 14:49:43 +0900

    make observered ptn

---
+++ Observer/src/weatherStation/WeatherData.java
@@ -0,0 +1,71 @@
+package weatherStation;
+
+import java.util.ArrayList;
+
+public class WeatherData  implements Subject {
+	
+
+
+	private ArrayList<Observer> observers;
+	private float temp;
+	private float humidity;
+	private float pressure;
+	
+	public float getTemp() {
+		return temp;
+	}
+
+	public void setTemp(float temp) {
+		this.temp = temp;
+	}
+
+	public float getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(float humidity) {
+		this.humidity = humidity;
+	}
+
+	public float getPressure() {
+		return pressure;
+	}
+
+	public void setPressure(float pressure) {
+		this.pressure = pressure;
+	}
+	
+	public void setMeasurements(float temp,float humidity, float pressure) {
+		this.temp = temp;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+		
+	}
+	public void measurementsChanged() {
+		notifyObserver();
+	}
+	
+	@Override
+	public void registerObserver(Observer o) {
+		// TODO Auto-generated method stub
+		this.observers.add(o);
+		
+	}
+	
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+		int i =  this.observers.indexOf(o);
+		if( i  >= 0 ) this.observers.remove(o);
+	}
+
+	@Override
+	public void notifyObserver() {
+		// TODO Auto-generated method stub
+		for ( Observer o: this.observers) {
+			o.update(temp,humidity,pressure);
+		}
+	}
+
+}
